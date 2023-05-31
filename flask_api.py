42b9d74bcab66897423175e26ac5138d616bb08b
nasslsr <117176437+nasslsr@users.noreply.github.com>
2023-05-31 21:39:13 +0200

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -4,20 +4,20 @@ from collect_data import collect_and_merge_data
 
 app = Flask(__name__)
 
-# Configuration de l'application Flask pour utiliser la base de données MongoDB
-app.config["MONGO_URI"] = "mongodb://localhost:27017/azerty"
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Velib"
 mongo = PyMongo(app)
 
-# Définition d'une route pour récupérer toutes les données dans la collection MongoDB
+
 @app.route("/velib_api_data", methods=["GET"])
 def get_data():
     dictionnary = collect_and_merge_data()
-    for i in mongo.db.bled.find():
+    for i in mongo.db.Data.find():
         i.pop("_id")  # Supprimez l'ID de MongoDB des données de sortie
         dictionnary.append(i)
     return jsonify(dictionnary)
 
-# Définition d'une route pour réinitialiser les données dans la collection MongoDB
+
 
 if __name__ == "__main__":
     app.run(debug=True)
